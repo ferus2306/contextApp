b518f1806c6623b7976844191564c46cd5746cef
Firus Hanov <d00363593@dmail.dixie.edu>
2020-05-26 16:38:48 -0600

    authentication added and AuthContext provided

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import Navbar from './components/Navbar'
 import BookList from './components/BookList'
 import ThemeContextProvider from './contexts/ThemeContext';
 import ThemeToggle from './components/ThemeToggle';
+import AuthContextProvider from './contexts/AuthContext';
 
 function App() {
   return (
     <div className="App">
 
     {/* Now items inside ThemeContextProvider are passed as props in other parts of components */}
+    
     <ThemeContextProvider>
-      <Navbar/>
-      <BookList />
-      <ThemeToggle />
+      <AuthContextProvider>
+        <Navbar/>
+        <BookList />
+        <ThemeToggle />
+      </AuthContextProvider>
     </ThemeContextProvider>
+    
     </div>
   );
 }
